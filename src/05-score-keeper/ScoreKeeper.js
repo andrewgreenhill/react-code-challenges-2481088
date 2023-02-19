60895ca14078c8aeab4a4e29cc5fe756881c6f6b
andrewgreenhill <andrewgggg2@gmail.com>
2023-02-19 14:48:36 +0800

    React.js Code Challenge #5

--- src/05-score-keeper/ScoreKeeper.js
+++ src/05-score-keeper/ScoreKeeper.js
@@ -1,13 +1,21 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from "react";
 
-export default function ScoreKeeper () {
-  const [score, setScore] = useState(0)
+export default function ScoreKeeper() {
+  const [score, setScore] = useState(
+    parseInt(localStorage.getItem("Score")) || 0
+  );
+
+  useEffect(() => localStorage.setItem("Score", score), [score]);
 
   return (
     <div>
       <h1>Your score is: {score}</h1>
-      <button onClick={() => setScore(prevScore => prevScore + 1)}>+</button>
-      <button onClick={() => setScore(prevScore => prevScore - 1)}>-</button>
+      <button onClick={() => setScore((prevScore) => prevScore + 1)}>+</button>
+      <button onClick={() => setScore((prevScore) => prevScore - 1)}>-</button>
     </div>
-  )
+  );
 }
+
+// Examples of using localStorage:
+//  localStorage.setItem("testA","AndrewG")
+//  localStorage.getItem("testA")
