d3bee82ef11c9900d1d4964af9d7c1dce7c55203
andrewgreenhill <andrewgreenhill@users.noreply.github.com>
2023-02-24 22:29:02 +0800

    React.js Code Challenge #10

--- src/10-focus-input/FocusInput.js
+++ src/10-focus-input/FocusInput.js
@@ -1,8 +1,14 @@
+import { useEffect, useRef } from "react";
+
 export default function FocusInput() {
+  const inputFieldRef = useRef(null);
+
+  useEffect(() => inputFieldRef.current.focus(), []); // Using useEffect to do the focussing upon page load :-)
+
   return (
     <div>
-      <label htmlFor='focused-input'>Focus me on page load!</label>
-      <input name='focused-input'></input>
+      <label htmlFor="focused-input">Focus me on page load!</label>
+      <input ref={inputFieldRef} name="focused-input"></input>
     </div>
-  )
+  );
 }
